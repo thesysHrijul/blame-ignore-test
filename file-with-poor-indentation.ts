b0a3cd5cadffb08b26e0840459cfe3f54ace77b2
Hrijul Bhatnagar <hrijul@thesys.dev>
2024-07-29 12:58:38 +0530

    Add a file with single line which should be multiline

---
+++ file-with-poor-indentation.ts
@@ -0,0 +1,9 @@
+function myFunction() {
+  console.log('This function has an indentation level of 2 spaces');
+  mySecondFunctionWithLotsOfParams( 'hello', 'this', 'is', 'my', 'function');
+}
+
+function mySecondFunctionWithLotsOfParams(a: string, b: string, c: string, d: string, e: string) {
+  console.log(`The passed params are: ${a}, ${b}, ${c}, ${d}, ${e}`);
+}
+
